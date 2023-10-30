c39ac47cbc8ea160bcc9358adf55ce4f6bd4c508
田小乔 <tianxiaoqiao@gitlab.com>
2023-10-30 10:26:48 +0800

    fix:修正格式

--- packages/plugins/@nocobase/plugin-theme-editor/src/server/builtinThemes.ts
+++ packages/plugins/@nocobase/plugin-theme-editor/src/server/builtinThemes.ts
@@ -5,28 +5,28 @@ export const defaultTheme: Omit<ThemeItem, 'id'> = {
   config: {
     "name": "Default",
     "token": {
-        "colorInfo": "#1677ff",
-        "wireframe": false,
-        "colorBorder": "#e5e5e5",
-        "colorWarning": "#FA8C16",
-        "colorBgHeader": "#001529",
-        "colorBgLayout": "#f2f2f2",
-        "colorSettings": "#4096ff",
-        "colorTextBase": "#000",
-        "colorPrimaryHeader": "#001529",
-        "colorTextHeaderMenu": "#ffffffa6",
-        "colorBgSettingsHover": "#4096ff0f",
-        "colorBorderSecondary": "#e0e0e0",
-        "colorBgHeaderMenuHover": "#ffffff1a",
-        "colorBgHeaderMenuActive": "#ffffff1a",
-        "colorBorderSettingsHover": "#4096ff4d",
-        "colorTextHeaderMenuHover": "#ffffff",
-        "colorTextHeaderMenuActive": "#ffffff"
+      "colorInfo": "#1677ff",
+      "wireframe": false,
+      "colorBorder": "#e5e5e5",
+      "colorWarning": "#FA8C16",
+      "colorBgHeader": "#001529",
+      "colorBgLayout": "#f2f2f2",
+      "colorSettings": "#4096ff",
+      "colorTextBase": "#000",
+      "colorPrimaryHeader": "#001529",
+      "colorTextHeaderMenu": "#ffffffa6",
+      "colorBgSettingsHover": "#4096ff0f",
+      "colorBorderSecondary": "#e0e0e0",
+      "colorBgHeaderMenuHover": "#ffffff1a",
+      "colorBgHeaderMenuActive": "#ffffff1a",
+      "colorBorderSettingsHover": "#4096ff4d",
+      "colorTextHeaderMenuHover": "#ffffff",
+      "colorTextHeaderMenuActive": "#ffffff"
     }
   },
   optional: true,
   isBuiltIn: true,
-  uid: 'default',
+  uid: 'default'
 };
 
 export const dark: Omit<ThemeItem, 'id'> = {
@@ -37,7 +37,7 @@ export const dark: Omit<ThemeItem, 'id'> = {
   },
   optional: true,
   isBuiltIn: true,
-  uid: 'dark',
+  uid: 'dark'
 };
 
 export const compact: Omit<ThemeItem, 'id'> = {
@@ -48,7 +48,7 @@ export const compact: Omit<ThemeItem, 'id'> = {
   },
   optional: true,
   isBuiltIn: true,
-  uid: 'compact',
+  uid: 'compact'
 };
 
 /** 同时包含 `紧凑` 和 `暗黑` 两种模式 */
@@ -60,5 +60,5 @@ export const compactDark: Omit<ThemeItem, 'id'> = {
   },
   optional: true,
   isBuiltIn: true,
-  uid: 'compact_dark',
+  uid: 'compact_dark'
 };
