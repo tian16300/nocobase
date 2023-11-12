a588b5b3708962c3da06c2bff007baa5d5e6aedb
田小乔 <tianxiaoqiao@gitlab.com>
2023-11-12 16:44:55 +0800

    fix:分类增加

--- packages/core/client/src/schema-component/antd/group-table/components/GroupTree.tsx
+++ packages/core/client/src/schema-component/antd/group-table/components/GroupTree.tsx
@@ -141,8 +141,6 @@ export const GroupTreeView = (props: any) => {
     <TreeView {...props} 
      expandedKeys={expandedKeys}
      onExpand={setExpandedKeys}
-    //  onSelect={onSelect}  
-    //  setExpandedKeys={setExpandedKeys}
      ></TreeView>
   );
 };
