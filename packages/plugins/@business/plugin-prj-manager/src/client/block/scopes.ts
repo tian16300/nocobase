d80efa946780d1c0086d2fe3ee315960b57732aa
tianxiaoqiao <hubeitxq@126.com>
2024-01-18 21:56:18 +0800

    fix: 修改区块边距问题

--- packages/plugins/@business/plugin-prj-manager/src/client/block/scopes.ts
+++ packages/plugins/@business/plugin-prj-manager/src/client/block/scopes.ts
@@ -1,31 +1,36 @@
-
 import { useCallback } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { TabsProps } from 'antd';
 import { css } from '@nocobase/client';
 export const usePrjTabsProps = () => {
-    const [searchParams, setSearchParams] = useSearchParams();   
-    const onTabChange = useCallback<TabsProps['onChange']>(
-        (key) => {
-          const prjId = searchParams.get('id');
-          const params:{ tab:string, id?:string } = {
-            tab: key
-          };
-          if(prjId){
-            params.id = prjId;
-          }
-          setSearchParams(params);
-        },
-        [setSearchParams],
-      );
-    const activeKey = searchParams.get('tab');
+  const [searchParams, setSearchParams] = useSearchParams();
+  const onTabChange = useCallback<TabsProps['onChange']>(
+    (key) => {
+      const prjId = searchParams.get('id');
+      const params: { tab: string; id?: string } = {
+        tab: key,
+      };
+      if (prjId) {
+        params.id = prjId;
+      }
+      setSearchParams(params);
+    },
+    [setSearchParams],
+  );
+  const activeKey = searchParams.get('tab');
   return {
-    ...activeKey?{activeKey}:{},
+    ...(activeKey ? { activeKey } : {}),
     onChange: onTabChange,
     className: css`
-     .nb-block-item.ant-nb-card-item > .ant-card > .ant-card-body{
-       padding: 0
-     }
-    `
+      .ant-tabs-tabpane
+        > .nb-grid
+        > .nb-grid-row
+        > .nb-grid-col
+        > .nb-block-item.ant-nb-card-item
+        > .ant-card
+        > .ant-card-body {
+        padding: 0;
+      }
+    `,
   };
 };
