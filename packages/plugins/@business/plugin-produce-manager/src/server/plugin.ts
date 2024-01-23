39ad8ed3656e18ef13759a3efc3db69d07efb955
tianxiaoqiao <hubeitxq@126.com>
2024-01-23 16:16:25 +0800

    fix: 修改数据表权限

--- packages/plugins/@business/plugin-produce-manager/src/server/plugin.ts
+++ packages/plugins/@business/plugin-produce-manager/src/server/plugin.ts
@@ -491,8 +491,10 @@ export class PluginProduceManagerServer extends Plugin {
     this.app.acl.allow('prjWlCb_bomWl_mid', '*', 'public');
     this.app.acl.allow('cgApply_prjs', '*', 'public');
     this.app.acl.allow('bom_apply','*', 'loggedIn');
+    this.app.acl.allow('bom_wl_list','*', 'loggedIn');
     this.app.acl.allow('cg_apply','*', 'loggedIn');
     this.app.acl.allow('cg_wl_list','*', 'loggedIn');
+     this.app.acl.allow('prj_wl_cb','*', 'loggedIn');
     
   }
 
