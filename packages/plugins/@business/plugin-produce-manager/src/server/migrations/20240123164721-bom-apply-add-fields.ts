7f325382d59ba67920ace35ababa3f98b7616d89
tianxiaoqiao <hubeitxq@126.com>
2024-01-23 17:36:52 +0800

    fix: 增加字段

---
+++ packages/plugins/@business/plugin-produce-manager/src/server/migrations/20240123164721-bom-apply-add-fields.ts
@@ -0,0 +1,76 @@
+import { Migration } from '@nocobase/server';
+
+export default class extends Migration {
+  on = 'afterLoad'; // 'beforeLoad' or 'afterLoad'
+  appVersion = '<=0.19.0-alpha.4';
+
+  async up() {
+    // coding
+     // coding
+     const { db } = this.context;
+     const repo = db.getRepository('fields');
+     const createdBy = await repo.findOne({
+       filter: {
+         name: 'createdBy',
+         collectionName: 'bom_apply',
+       },
+     });
+     if (!createdBy) {
+       await repo.create({
+         values: {
+          name: 'createdBy',
+          type: 'belongsTo',
+          interface: 'createdBy',
+          collectionName: 'bom_apply',
+          description: null,
+          target: 'users',
+          foreignKey: 'createdById',
+          uiSchema: {
+            type: 'object',
+            title: '{{t("Created by")}}',
+            'x-component': 'AssociationField',
+            'x-component-props': {
+              fieldNames: {
+                value: 'id',
+                label: 'nickname',
+              },
+            },
+            'x-read-pretty': true,
+          },
+          targetKey: 'id',
+        }
+       });
+     }
+     const updatedBy = await repo.findOne({
+      filter: {
+        name: 'updatedBy',
+        collectionName: 'bom_apply',
+      },
+    });
+    if (!updatedBy) {
+      await repo.create({
+        values: {
+          name: 'updatedBy',
+          type: 'belongsTo',
+          interface: 'updatedBy',
+          collectionName: 'bom_apply',
+          target: 'users',
+          foreignKey: 'updatedById',
+          uiSchema: {
+            type: 'object',
+            title: '{{t("Last updated by")}}',
+            'x-component': 'AssociationField',
+            'x-component-props': {
+              fieldNames: {
+                value: 'id',
+                label: 'nickname',
+              },
+            },
+            'x-read-pretty': true,
+          },
+          targetKey: 'id',
+        }
+      });
+    }
+  }
+}
